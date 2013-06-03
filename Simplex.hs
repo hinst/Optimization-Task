6419bdb6bd65aafe90d7d4e381805a4346b6cd4a
hinst <hinstance@yandex.ru>
2013-06-03 18:30:44 +0400

    Updates for Simplex.hs module here. I changed the SimplexTask data type. Now it contains not generic elements, but elements of type Double instead. I added the defaultDecimalPlaces constant and the defaultVecdisp function in order to print vectors from this module in a convinient way. I changed sample task matrix size. Looks like it was incorrect in the previous state. I changed sample task constraint string from tuple to vector. The previous version was incorrect. I added getInclusiveVariable and getExclusiveVariable functions which choose variables to include and to exclude for a Simplex algorythm iteration. They seem to be in a pretty stable and partially usable state now. I run several manual tests for them with my sample task. Other changes: see source.

--- Simplex.hs
+++ Simplex.hs
@@ -1,19 +1,32 @@
 module Simplex
 (
+	getInclusiveVariable
 )
 where
  
 import Numeric.LinearAlgebra
 import Foreign.Storable
+import HMatrixUtil
 import MyTrace
 
-data SimplexTask t = SimplexTask (Matrix t) (Vector t)
+data SimplexTask = SimplexTask (Matrix Double) (Vector Double)
+
+defaultDecimalPlaces :: Int
+defaultDecimalPlaces = 2
+defaultVecdisp = vecdisp (dispf defaultDecimalPlaces)
+
+instance Show SimplexTask where
+	show (SimplexTask constraints zString) = 
+		"Constraints: "
+		++ dispf defaultDecimalPlaces constraints
+		++ "ZString: \n"
+		++ defaultVecdisp zString
 
 sampleTasks = 
 	[
 		(SimplexTask
 			(
-				(5 >< 7)
+				(4 >< 7)
 				[
 					 6, 4, 1, 0, 0, 0,  24,
 					 1, 2, 0, 1, 0, 0,  6,
@@ -21,22 +34,79 @@ sampleTasks =
 					 0, 1, 0, 0, 0, 1,  2
 				]
 			)
-			(-5, -4, 0, 0, 0, 0)
+			(
+				7 |> [-5, -4, 0, 0, 0, 0, 0]
+			)
 		)
 	]
 
-simplex :: (Product t) => SimplexTask t -> Vector t
-simplex task =
-	0 |> ([]::[t])
+--this function returns column index
+getInclusiveVariable :: SimplexTask -> Int
+getInclusiveVariable (SimplexTask constraints zString) =
+	resultIndex
+	where
+		index = searchVector zString vectorChooseMin
+		value = zString @> index
+		resultIndex = 
+			if
+				value < 0
+			then
+				index
+			else
+				-1
+
+--this function returns row index
+getExclusiveVariable :: SimplexTask -> Int -> Int
+getExclusiveVariable (SimplexTask constraints zString) inclusiveVariableIndex =
+	resultIndex
+	where
+		traceEnabled = False
+		trace logMessage expression = 
+			if 
+				traceEnabled
+			then
+				MyTrace.trace logMessage expression
+			else
+				expression
+		resultIndex = 
+			if 
+				inclusiveVariableIndex /= -1
+			then
+				bestRatio			
+			else
+				-1
+		inclusiveVariableColumn = getColumn inclusiveVariableIndex constraints
+		rightColumn = getColumn (cols constraints - 1) constraints
+		ratioColumn = 
+			trace
+				(
+					defaultVecdisp rightColumn 
+					++ defaultVecdisp inclusiveVariableColumn 
+					++ defaultVecdisp result
+				)
+				result
+			where
+				result = mapVectorWithIndex evaluateRatio inclusiveVariableColumn
+				evaluateRatio index inclusiveValue
+					| inclusiveValue == 0 = -1
+					| otherwise = rightValue / inclusiveValue
+					where
+						rightValue = rightColumn @> index
+		bestRatio = 
+			searchVectorS ratioColumn chooseRatio
+		chooseRatio a b
+			| (a > 0) && (b > 0) = if a <= b then 0 else 1
+			| (a > 0) && (b <= 0) = 0
+			| (a <= 0) && (b > 0) = 1
+			| (a < 0) && (b < 0) = -1
+
+simplex :: SimplexTask -> Vector Double
+simplex task@(SimplexTask constraints zString) =
+	zString
 	
-evaluate :: (Product t) => Vector t -> Vector t -> t
+evaluate :: Vector Double -> Vector Double -> Double
 evaluate function arguments = function <.> arguments
 	
 findInclusiveVariable :: Vector t -> Int
 findInclusiveVariable target =
 	0
-	
-
-	
-					
-			
